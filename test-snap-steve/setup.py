d5fe24b4a3a8965db2a492ff11848ce2ede839e4
Steve Rydz <steve.rydz@canonical.com>
2021-01-07 14:58:28 +0000

    Initial commit

---
+++ test-snap-steve/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="test-snap-fran",
+    version="0.0.2",
+    author="Francisco Jimenez Cabrera",
+    author_email="francisco.jimenez.cabrera@canonical.com",
+    py_modules=["test_snap"],
+)
---
+++ test-snap-steve/test_snap.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from test_snap.main import run
+
+run()
